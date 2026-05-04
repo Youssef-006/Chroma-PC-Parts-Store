93fb817c0e80bbc00c5d9f97fc388f1c2f7faa8b
Youssef <youssefashoor26@gmail.com>
2026-05-04 12:00:34 +0300

    Updated application.properties to make sure it's in the correct path for our database, updated User.java class with the ID and more lombko annotations

---
+++ src/main/java/com/chroma/chroma/model/User.java
@@ -0,0 +1,37 @@
+package com.chroma.chroma.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "First Name is required")
+    @Column(nullable = false)
+    private String firstName;
+
+    @NotBlank(message = "Last Name is required")
+    @Column(nullable = false)
+    private String lastName;
+
+    @Email(message = "Enter a valid email")
+    @NotBlank(message = "Email is required")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Column(nullable = false)
+    private String password;
+}
