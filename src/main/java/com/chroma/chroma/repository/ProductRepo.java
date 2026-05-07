6aff6203d1befb6a86f1514ab6155fc2415517b6
ProductController <mohamudhassan0406@gmail.com>
2026-05-07 12:35:37 +0300

    made ProductRepo.java

--- src/main/java/com/chroma/chroma/repository/ProductRepo.java
+++ src/main/java/com/chroma/chroma/repository/ProductRepo.java
@@ -3,5 +3,5 @@ package com.chroma.chroma.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.chroma.chroma.model.Product;
 
-public interface ProductRepo extends JpaRepository<Product, Long> {
+public interface ProductRepo extends JpaRepository<Product  ,   Long> {
 }
